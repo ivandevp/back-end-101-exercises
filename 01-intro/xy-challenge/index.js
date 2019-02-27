8219f7bc7784f0ac654004b81ff65abd2c81e642
shirley-startary <shirley.startary@gmail.com>
2019-02-26 19:41:53 -0600

    agrega solución de reto 1 readFile ,falta HE - Silvana

---
+++ 01-intro/xy-challenge/index.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const questionPromise = message =>
+    new Promise((resolve, reject) => {
+        rl.question(message, answer => {
+            resolve(answer);
+        });
+    });
+
+const readFilePromise = (file, options) =>
+    new Promise((resolve, reject) => {
+        fs.readFile(file, options, (error, data) => {
+            if (error) {
+                reject(`Ocurrió un error: ${error.message}`);
+                return;
+            }
+            resolve(data);
+        });
+    });
+
+const exists = (answer, dataFile) => (
+    (dataFile.indexOf(answer) !== -1) ?
+    true :
+    false
+);
+
+const init = async (message, file, options) => {
+    const answer = await questionPromise(message);
+    console.log(answer);
+    rl.close();
+    const dataFile = await readFilePromise(file, options);
+    console.log(dataFile);
+    const result = exists(answer, data);
+    console.log(result);
+};
+
+init("Escribe algo que quieras buscar: ", "texto.txt", {
+    encoding: "utf8"
+});
