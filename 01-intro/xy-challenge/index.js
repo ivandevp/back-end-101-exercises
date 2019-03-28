698241d33a9e13fbbcda89af03d7d4b40ee57881
YocelinGR <garcia_romero.y@hotmail.com>
2019-03-28 00:36:06 -0600

    get merge

--- 01-intro/xy-challenge/index.js
+++ 01-intro/xy-challenge/index.js
@@ -9,15 +9,16 @@ const rl = readline.createInterface({
 rl.question('Escribe algo que quieras buscar: ', (answer) => {
     console.log(`Buscando: ${answer} ...`);
     const path = __dirname;
-    fs.readFile(`${path}\\prueba.txt`, 'utf8', (err, data) => {
+    const character = require('path');
+    fs.readFile(character.join(path, 'prueba.txt'), 'utf8', (err, data) => {
         if (err) {
             console.error(`Hubo un error ${err.message}`);
             return;
         }
-        let expReg = new RegExp(answer, "gi");
+        const expReg = new RegExp(answer, "gi");
         const searched = data.match(expReg);
-        if (searched == null || searched == 0) {
-            console.log('No encntrado');
+        if (searched === null || searched === 0) {
+            console.log('No encontrado');
         } else {
             console.log(`${answer} aparece: ${searched.length} veces`);
         }
